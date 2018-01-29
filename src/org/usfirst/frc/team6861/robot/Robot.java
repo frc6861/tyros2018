d5e5f5a78c10d8b5e96ea61104d5e3749633fdf8
Ethan <ethompso@livoniawarriors.org>
2018-01-29 18:47:24 -0500

    Added webcam support

--- src/org/usfirst/frc/team6861/robot/Robot.java
+++ src/org/usfirst/frc/team6861/robot/Robot.java
@@ -34,8 +34,9 @@ public class Robot extends TimedRobot {
 		
 	/**
 	 * This function is run when the robot is first started up and should be
-	 * used for any initialization code.
+	 * used for any initialization code. 
 	 */
+
 	@Override
 	public void robotInit() {
 		m_oi = new OI();
